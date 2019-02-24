8bd8465dbd54541d0918f3b2d657f932df96029d
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-02-23 23:29:50 -0500

    use wordnik api to open the first audio file in browser and pronounce the name of the word

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -7,35 +7,38 @@ word = gets.chomp
 while word != "q"
 
 # use wordnik API to show the words definition
-definition = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+  definition = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
 
-word_data = definition.parse # This is a loop you can loop through
-p "#{word} can be defined as:"
-i = 0
-j = 1
-word_data.length.times do
-  word_definition = word_data[i]["text"] 
-  p "#{j}. #{word_definition}"
-  i += 1
-  j += 1
-end
+  word_data = definition.parse # This is a loop you can loop through
+  p "#{word} can be defined as:"
+  i = 0
+  j = 1
+  word_data.length.times do
+    word_definition = word_data[i]["text"] 
+    p "#{j}. #{word_definition}"
+    i += 1
+    j += 1
+  end
 
 
-# use wordnik API to show the top example of a word
-top_example_data = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/topExample?useCanonical=false&api_key=")
+  # use wordnik API to show the top example of a word
+  top_example_data = HTTP.get("https://api.wordnik.com/v4/word.json/tree/topExample?useCanonical=false&api_key=")
 
-top_example = top_example_data.parse
-p "Here is the top example for the usage of the word #{word}:"
-p top_example["text"]
+  top_example = top_example_data.parse
+  p "Here is the top example for the usage of the word #{word}:"
+  p top_example["text"]
 
-# use wordnik API to show the pronounciation of a word
-pronunciation = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=")
-pronunciation_data = pronunciation.parse
-how_to_pronounce = pronunciation_data[0]["raw"]
-p "Here is how you pronounce the word #{word}"
-p how_to_pronounce
+  # use wordnik API to show the pronounciation of a word
+  pronunciation = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=")
+  pronunciation_data = pronunciation.parse
+  how_to_pronounce = pronunciation_data[0]["raw"]
+  p "Here is how you pronounce the word #{word}"
+  p how_to_pronounce
+  audio = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/audio?useCanonical=false&limit=50&api_key=")
+  audio_data = audio.parse
+  system("open","#{audio_data[0]["fileUrl"]}")
 
-# ask user to enter a word
-p "Please enter a word (you may also enter 'q' to quit):"
-word = gets.chomp
+  # ask user to enter a word
+  p "Please enter a word (you may also enter 'q' to quit):"
+  word = gets.chomp
 end
